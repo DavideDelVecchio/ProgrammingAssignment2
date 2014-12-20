56fa091fd082a1a547c6bcb0d2c8223e4831c462
davide.delvecchio <davide.delvecchio@gmail.com>
2014-12-20 11:28:31 +0100

    corrected a typo and added example of invocation

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
 ## The makeCacheMatrix constructs a suitable object that can store the value 
 ## of both the direct and cached inverse it should be used in combination with cacheSolve
 ## that when called before perforimg the computation of the inverse checks for the cached variable
-## if not found computes the inverse and cache it 
+## if not found computes the inverse and caches it in the object returned by makeCacheMatrix
+## usage example
+## > set.seed(44)
+## > m <- matrix(sample.int(100,size=9,replace=TRUE), nrow=3)
+## > d <- makeCacheMatrix(m)
+## > cacheSolve(d)
+## > subsequent  invocation returns cached data
+## > inv <- cacheSolve(d)
+## returning cached data
 
 ## makeCacheMatrix stores in variable i the inverse and provides accessor methods for both 
 ## the data (ie the matrix one wants to calculate the inverse) and the inverse itself
